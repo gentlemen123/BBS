28ecdda76da4b79354a50603c39110336ed5d8b5
youjingcheng <2505034080@qq.com>
2017-07-17 16:32:22 +0800

    完成page内所有文章链接获取

--- article_link_crawler.py
+++ article_link_crawler.py
@@ -7,14 +7,113 @@
 @email：2505034080@qq.com
 """
 
+import sys
+import json
+import time
+import requests
+from lxml import html
+from bs4 import BeautifulSoup
 
-def get_article_links():
-    """
+from data_storage import insert_data
+from data_storage import find_data
 
-    :return: 一个列表！包含一个page内所有文章链接.
+
+
+def get_article_links(page_url):
     """
-    return
+    获取一个论坛页面所有普通主题的文章链接！
+    将值存入mongodb中
+    :param page_url: 论坛页面url
+    :return:
+    """
+    print("\ngo to page:", page_url)
+    with open(".progress.json", "w+") as f:
+        json.dump({
+            'page': page_url
+        }, f)
+
+    headers = {
+        'Pragma': 'no-cache',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/57.0.2987.133 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Referer': 'http://bbs.cloud.icybee.cn/board/JobInfo',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache',
+    }
+
+    try:
+        a = time.time()  # 发出请求的时间
+        response = requests.get(page_url, headers=headers)
+        b = time.time()  # 请求结束的时间
+        print(b-a)  # 输出一次请求所需的时间
+    except TimeoutError as e:
+        print(e)
+        sys.exit(1)
+    except ConnectionError as e:
+        print(e)
+        print("页面url出错")
+    else:
+        if response.status_code != 200:
+            print('code != 200')
+            sys.exit(1)
+        if "招聘|兼职" not in response.text:
+            print('not in the right page')
+            print('current page:', page_url)
+            sys.exit(1)
+
+        tree = html.fromstring(response.text) # "//table[@class='mainbox tableborder']/tbody/tr[@class='trout']"
+        article_links = tree.xpath("//form/table[@class='mainbox tableborder']/tr[@onmouseover]")
+        print(article_links)
+        print("count:", len(article_links))
+        for article_link in article_links:
+            article_link = html.fromstring(html.tostring(article_link))
+
+            # 文章最后回复时间
+            update_time = article_link.xpath("//td[5]/a/text()")[0]
+            if '2017-7-5' in update_time:
+                print(update_time)
+                print('done')
+                return True
+
+            # 文章发表时间
+            publish_time = article_link.xpath('//td[3]/a/text()')[1].strip()
+            if ':' not in publish_time:
+                p = publish_time.split('-')
+                year = p[0]
+                month = p[1]
+                day = p[2]
+                if year != '2017':
+                    continue
+                if int(month) < 7 and int(day) < 5:
+                    continue
+            else:
+                publish_time = '2017-07-17'
+
+            link = article_link.xpath("//td[2]/a/@href")
+            title = article_link.xpath("//td[2]/a/text()")
+            if link and title:
+                link = link[0]
+                title = title[0].split()
+            else:
+                break
+            id = link.split('=')[-1]
+            link = 'http://www.xgbbs.net/xgbbs/' + link
+            article_json = {
+                'id': id,
+                'link': link,
+                'title': title,
+                'publishTime': publish_time,
+                'status': 'not_done'
+            }
+            if not find_data({'id': id}):
+                insert_data(article_json)
+    return True
 
 
 if __name__ == "__main__":
-    get_article_links()
+    page_url = "http://www.xgbbs.net/xgbbs/index.asp?boardid=56&TopicMode=0&List_Type=&Page=1"
+    get_article_links(page_url)
--- data_storage.py
+++ data_storage.py
@@ -8,11 +8,16 @@
 """
 from pymongo import MongoClient
 
-def insert_data():
+
+def insert_data(article_json):
     """
     插入数据
+    :param article_json: json数据，包含id,link,title,publish_time,status
     :return:
     """
+    db = MongoClient().get_database('beijingwaiguoyu')
+    col = db.get_collection('articles')
+    col.insert_one(article_json)
     return
 
 
@@ -23,6 +28,16 @@ def update_data():
 def del_data():
     return
 
+
+def find_data(pattern_json):
+    """
+    在mongodb中找到和pattern_json匹配成功的文件
+    :param pattern_json: json数据，查找的条件
+    :return: boolean数据
+    """
+    db = MongoClient().get_database('beijingwaiguoyu')
+    col = db.get_collection('articles')
+    return col.find_one(pattern_json)
 
 if __name__ == "__main__":
     insert_data()
