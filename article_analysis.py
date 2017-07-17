a8d921dcb2b4f6cf1f11d9c6b89a140207cea55b
youjingcheng <2505034080@qq.com>
2017-07-17 18:29:16 +0800

    完成文章解析,获取email,电话信息的获取

--- article_analysis.py
+++ article_analysis.py
@@ -6,15 +6,138 @@
 @author：jingchengyou
 @email：2505034080@qq.com
 """
+import re
+from pymongo import MongoClient
 
 
-
+# 存在两个问题:1.article的值可能是列表形式  2.是否删除article的值
 def analysis_article():
-    """
+    db = MongoClient().get_database('beijingwaiguoyu')
+    col = db.get_collection('articles')
 
-    :return: 一个列表！包含文章中邮箱、电话、时间三个信息
-    """
-    return
+    total = col.count()
+    i = 1
+    while True:
+        document = col.find_one({'status': 'done'})
+        if not document:
+            break
+        try:
+            id = document['id']
+        except:
+            print(document)
+            _id = document['_id']
+            col.find_one_and_update({'_id': _id}, {'$set': {'status': 'fail'}})
+            continue
+        try:
+            text = str(document['article'])
+            # link = document['link']
+        except:
+            print(id)
+            col.find_one_and_update(
+                {'id': id},
+                {'$set': {
+                    'status': 'not_done'
+                }}
+            )
+            continue
+        contact = {
+            'phone': [],
+            'email': []
+        }
+
+        while True:
+            # telephone
+            telephone_pattern = re.compile(r'0\d{2,3}-\d{7,8}|0\d{2,3}\d{7,8}|1[358]\d{9}|147\d{8}')
+            # print(text)
+            t = re.search(telephone_pattern, text)
+            if t:
+                contact['phone'].append(t.group())
+                text = text.replace(t.group(), '')
+            else:
+                break
+
+        while True:
+            # email
+            email_pattern = r'([A-Z_a-z_0-9.-]{1,64}@[a-z0-9-]{1,200}.{1,5}[a-z]{1,6})'
+            e = re.search(email_pattern, text)
+            if e:
+                contact['email'].append(e.group(1))
+                text = text.replace(e.group(1), '')
+                continue
+
+            email_pattern = r'([A-Z_a-z_0-9.-]{1,64}#[a-z0-9-]{1,200}.{1,5}[a-z]{1,6})'
+            e = re.search(email_pattern, text)
+            if e:
+                contact['email'].append(e.group(1).replace('#', '@'))
+                text = text.replace(e.group(1), '')
+                continue
+
+            email_pattern = r'([A-Z_a-z_0-9.-]{1,64} # [a-z0-9-]{1,200}.{1,5}[a-z]{1,6})'
+            e = re.search(email_pattern, text)
+            if e:
+                contact['email'].append(e.group(1).replace(' # ', '@'))
+                text = text.replace(e.group(1), '')
+                continue
+
+            email_pattern = r'([A-Z_a-z_0-9.-]{1,64} At [a-z0-9-]{1,200}.{1,5}[a-z]{1,6})'
+            e = re.search(email_pattern, text)
+            if e:
+                contact['email'].append(e.group(1).replace(' At ', '@'))
+                text = text.replace(e.group(1), '')
+                continue
+
+            email_pattern = r'([A-Z_a-z_0-9.-]{1,64}##[a-z0-9-]{1,200}.{1,5}[a-z]{1,6})'
+            e = re.search(email_pattern, text)
+            if e:
+                contact['email'].append(e.group(1).replace('##', '@'))
+                text = text.replace(e.group(1), '')
+                continue
+
+            email_pattern = r'([A-Z_a-z_0-9.-]{1,64}#_#[a-z0-9-]{1,200}.{1,5}[a-z]{1,6})'
+            e = re.search(email_pattern, text)
+            if e:
+                contact['email'].append(e.group(1).replace('#_#', '@'))
+                text = text.replace(e.group(1), '')
+                continue
+
+            email_pattern = r'([A-Z_a-z_0-9.-]{1,64} AT [a-z0-9-]{1,200}.{1,5}[a-z]{1,6})'
+            e = re.search(email_pattern, text)
+            if e:
+                contact['email'].append(e.group(1).replace(' AT ', '@'))
+                text = text.replace(e.group(1), '')
+                continue
+
+            email_pattern = r'([A-Z_a-z_0-9.-]{1,64} at [a-z0-9-]{1,200}.{1,5}[a-z]{1,6})'
+            e = re.search(email_pattern, text)
+            if e:
+                contact['email'].append(e.group(1).replace(' at ', '@'))
+                text = text.replace(e.group(1), '')
+                continue
+
+            break
+
+        if contact['phone'] or contact['email']:
+            # time.sleep(0.2)
+            print('{} / {}, {:.2f}%'.format(i, total, 100*i/total))
+            print(contact)
+            # time.sleep(1)
+            col.find_one_and_update(
+                {'id': id},
+                {'$set': {
+                    'contact': contact,
+                    'status': 'fetched'
+                }}
+            )
+        else:
+            col.find_one_and_update(
+                {'id': id},
+                {'$set': {
+                    'contact': {},
+                    'status': 'fetched'
+                }}
+            )
+
+        i += 1
 
 
 if __name__ == "__main__":
