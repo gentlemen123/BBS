29c5443efb60f65bc936ac915ae568f79400343e
youjingcheng <2505034080@qq.com>
2017-07-24 19:33:33 +0800

    添加配置文件,将headers, cookies等变量添加进去,进一步降低耦合性

--- article_link_crawler.py
+++ article_link_crawler.py
@@ -15,6 +15,7 @@ from lxml import html
 
 from data_storage import find_data
 from data_storage import insert_data
+from config import headers
 
 
 def get_article_links(page_url):
@@ -29,19 +30,6 @@ def get_article_links(page_url):
         json.dump({
             'page': page_url
         }, f)
-
-    headers = {
-        'Pragma': 'no-cache',
-        'Accept-Encoding': 'gzip, deflate, sdch',
-        'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/57.0.2987.133 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Referer': 'http://bbs.cloud.icybee.cn/board/JobInfo',
-        'Connection': 'keep-alive',
-        'Cache-Control': 'no-cache',
-    }
 
     try:
         a = time.time()  # 发出请求的时间
--- webpage_crawler.py
+++ webpage_crawler.py
@@ -8,6 +8,7 @@
 """
 import requests
 from lxml import html
+from config import headers
 
 
 def get_page_url(base_url):
@@ -24,18 +25,6 @@ def get_page_url_number(base_url):
     :param base_url: 论坛首页url
     :return: 最大数目,int型
     """
-    headers = {
-        'Pragma': 'no-cache',
-        'Accept-Encoding': 'gzip, deflate, sdch',
-        'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/57.0.2987.133 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Referer': 'http://bbs.cloud.icybee.cn/board/JobInfo',
-        'Connection': 'keep-alive',
-        'Cache-Control': 'no-cache',
-    }
     response = ""
     try:
         response = requests.get(base_url, headers=headers)
